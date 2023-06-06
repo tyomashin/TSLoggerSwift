f9b479fd0b82c985cad1364c97cbaa55d7d4763c
Shinya Okazaki <negichou@gmail.com>
2023-06-07 03:07:35 +0900

    add sample project

---
+++ Sample/TSLoggerSwiftSample/TSLoggerSwiftSample/ViewController.swift
@@ -0,0 +1,14 @@
+// Created by okazakishinya on 2023/06/07.
+
+import UIKit
+import TSLoggerSwift
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
